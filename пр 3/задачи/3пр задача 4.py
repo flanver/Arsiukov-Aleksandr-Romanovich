c0eae19b8009e7f1eb7481c1c6429dab6123cb0e
Arsiukov Aleksandr Romanovich <arsiukov.sasha@yandex.ru>
2022-10-09 22:01:32 +0300

    4

---
+++ пр 3/задачи/3пр задача 4.py
@@ -0,0 +1,8 @@
+def F(a, b, l, N):
+    return (a*2+b*2)*(N-1)+l*2+a
+
+a=int(input()) #  a расстояние между рядами
+b=int(input()) #  b расстояние между отверстиями
+l=int(input()) #  l длина ост 2 шнурков
+N=int(input()) #  N кол-во отверстий
+print(F(a, b, l, N))
