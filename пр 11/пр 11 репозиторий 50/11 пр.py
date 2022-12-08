3781b72fd00c68315dd6c34fa0630953e7271b16
Arsiukov Aleksandr Romanovich <arsiukov.sasha@yandex.ru>
2022-12-09 02:19:31 +0300

    +

--- пр 11/пр 11 репозиторий 50/11 пр.py
+++ пр 11/пр 11 репозиторий 50/11 пр.py
@@ -1,10 +1,27 @@
+from tkinter import *  
+from tkinter import messagebox
+import tkinter
 import requests
-from pprint import pprint
-# Имя пользователя github
-username = "kubernetes"
-# url для запроса
-url = f"https://api.github.com/users/{username}"
-# делаем запрос и возвращаем json
-user_data = requests.get(url).json()
-# довольно распечатать данные JSON
-pprint(user_data)
+import json
+
+
+def clicked():  
+    res = "Привет ".format(txt.get())  
+    lbl.configure(text=res)  
+    messagebox.showwarning('предупреждение', 'файл будет сохранен как только вы нажмете Ок')
+    return txt.get()
+def com():
+    with open ("C:\\Users\\fotog\\Desktop\\работы инфа\\Arsiukov-Aleksandr-Romanovich\\пр 11\\пр 11 репозиторий 50\\ddd.txt", 'w') as file:
+        file.write(str(clicked()))
+    window.destroy()
+window = Tk()  
+window.title("Добро пожаловать в приложение PythonRu")  
+window.geometry('400x250')  
+lbl = Label(window, text="Привет")  
+lbl.grid(column=0, row=0)  
+txt = Entry(window,width=10)  
+txt.grid(column=1, row=0)  
+btn = Button(window, text="Клик!", command=com)  
+btn.grid(column=2, row=0)  
+window.mainloop()
+
