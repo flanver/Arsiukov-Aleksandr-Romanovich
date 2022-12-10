22fe3f43b8ce655bfe54e17ce2f5a6398de18a11
Arsiukov Aleksandr Romanovich <arsiukov.sasha@yandex.ru>
2022-12-10 19:46:57 +0300

    +

--- пр 11/пр 11 репозиторий 50/11 пр.py
+++ пр 11/пр 11 репозиторий 50/11 пр.py
@@ -1,19 +1,45 @@
 from tkinter import *  
 from tkinter import messagebox
+import tkinter as tk 
 import tkinter
 import requests
 import json
 
 
 def clicked():  
-    res = "Привет ".format(txt.get())  
-    lbl.configure(text=res)  
+    v = txt.get()
+    response = requests.get('https://api.github.com/users/OwnCloud')
+
+    f_ = json.loads(response.text)
+
+    y = dict.fromkeys(['company'], f_['company'])
+    y2 = dict.fromkeys(['created_at'], f_['created_at'])
+    y3 = dict.fromkeys(['email'], f_['email'])
+    y4 = dict.fromkeys(['id'], f_['id'])
+    y5 = dict.fromkeys(['name'], f_['name'])
+    y6 = dict.fromkeys(['url'], f_['url'])
+    j = (y,y2,y3,y4,y5,y6)
+
+
+
+
+    
+
+    if v == 'OwnCloud':
+        with open('C:\\Users\\fotog\\Desktop\\работы инфа\\Arsiukov-Aleksandr-Romanovich\\пр 11\\пр 11 репозиторий 50\\vivod.json', 'w') as file:
+            json.dump(j,file)
+        messagebox.showwarning('предупреждение', 'файл будет сохранен как только вы нажмете Ок')
+        window.destroy()
+        
+        
+    else:
+        print('Данное имя не задано')
+    
+    
     messagebox.showwarning('предупреждение', 'файл будет сохранен как только вы нажмете Ок')
-    return txt.get()
-def com():
-    with open ("C:\\Users\\fotog\\Desktop\\работы инфа\\Arsiukov-Aleksandr-Romanovich\\пр 11\\пр 11 репозиторий 50\\ddd.txt", 'w') as file:
-        file.write(str(clicked()))
     window.destroy()
+
+
 window = Tk()  
 window.title("Добро пожаловать в приложение PythonRu")  
 window.geometry('400x250')  
@@ -21,7 +47,8 @@ lbl = Label(window, text="Привет")
 lbl.grid(column=0, row=0)  
 txt = Entry(window,width=10)  
 txt.grid(column=1, row=0)  
-btn = Button(window, text="Клик!", command=com)  
+btn = Button(window, text="Клик!", command=clicked)  
 btn.grid(column=2, row=0)  
 window.mainloop()
+
 
